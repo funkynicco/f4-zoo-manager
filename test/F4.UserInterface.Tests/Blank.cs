aaea6b2b392fd34bbce3074f640dd1d28ff9abbc
Nicco <funkynicco@gmail.com>
2020-01-17 18:31:05 +0100

    Initial commit

---
+++ test/F4.UserInterface.Tests/Blank.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace F4.UserInterface.Tests
+{
+    public class Blank
+    {
+        [Fact]
+        public void BankTest()
+        {
+        }
+    }
+}
---
+++ test/F4.Zoo.Tests/Blank.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace F4.Zoo.Tests
+{
+    public class Blank
+    {
+        [Fact]
+        public void BankTest()
+        {
+        }
+    }
+}
